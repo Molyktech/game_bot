33dea854e354429782ce57d15a0dfc505e27ffad
molyktech <modupeadebayo001@gmail.com>
2018-07-04 17:57:52 +0100

    pos

--- API/Routes/Routes.js
+++ API/Routes/Routes.js
@@ -7,25 +7,52 @@ module.exports = function(app) {
 var apiRoutes =  express.Router();
 
   app.get('/',function(req,res) {
-      res.send('Welcome to Paddy');
+      res.send('Welcome to Paddy Lift');
   });
 
-  // registerUser Route
-  app.get('/welcome', (req, res) => {
+  
+  app.post('/webhook', (req,res)=> {
+    console.log('receives as post request');
+    if(!req.body)
+      return res.sendStatus(400);
+    res.setHeader('Content-Type', 'application/json');
+    console.log('here is the post from dialogflow');
+    console.log(req.body);
+    let response = "" //default response from the webhook to show its working.
+    responseObj = {
+      "fufillmentText": response,
+      "fufillmentMessages":[{"text": {"text": [w]}}],
+      "source": ""
+    }
+    console.log('response from dialogflow');
+    console.log(responseObj);
+    return res.json(responseObj);
+
+  });
+
+ /* app.post('/welcome', (req, res) => {
     var speech =
     req.body.result &&
     req.body.result.parameters &&
-    req.body.result.parameters.fufillmentText
-      ? req.body.result.parameters.fufillmentText
+    req.body.result.parameters.action
+      ? req.body.result.parameters.action
       : "I do not understand that, please can you say that again.";
   return res.json({
     speech: speech,
     displayText: speech,
-    source: "Default Welcome Intent"
+    source: "action-codelabs"
   });
-});
+});*/
 
   
   
-  app.post('/ask', gameDataController.processRequest);
+  app.post('/ask', (req,res) => {
+    if (req.body.result.action == "input.welcome") {
+      return res.json({
+        speech: "hi there",
+        displayText: "hi there",
+        source: 'action-codelabs'
+    });
+  }
+  });
 };
